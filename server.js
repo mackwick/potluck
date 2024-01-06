ef5bb55a95aa6cf0298a02c2b3ce9887453dca4c
mackwick <mackenziewicker@gmail.com>
2024-01-06 12:58:44 -0500

    server is alive

---
+++ server.js
@@ -0,0 +1,29 @@
+//DEPENDENCIES
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+
+//ENV VARS
+const { DATABASE_URL, SECRET, PORT } = process.env;
+
+//DB CONNECTION
+mongoose.connect(DATABASE_URL);
+mongoose.connection
+  .on("open", () => console.log("Mongo connected"))
+  .on("closed", () => console.log("Mongo disconnected"))
+  .on("error", (error) => console.log(error));
+
+//APP OBJ
+const app = express();
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.send("It's working");
+});
+
+//SERVER LISTENER
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
+});
