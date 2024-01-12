16d583ed9f539a37e24d53866030d216b6dde914
mackwick <mackenziewicker@gmail.com>
2024-01-12 18:23:19 -0500

    started trying a css thing but I should really do more research first

---
+++ public/app.js
@@ -0,0 +1,7 @@
+// <% const colorRando = ["18191C", "8F4D2B", "B36A3E", "BD9D89", "E5C8B3",
+// "CFCAC6"]%> let boxColor = colorRando[Math.floor(Math.random() *
+// colorRando.length)] %> const catThoughts = ["...human...why you taking pictures
+// of me?...", "...the catnip made me do it...", "...why does the red dot always
+// get away..."] for (let i = 1; i < 21; i++) { const randomCat =
+// catThoughts[Math.round(Math.random() * 2)] if (i % 2 === 0) {
+// console.log(randomCat) } else {console.log("Love me, pet me! HSSSSSSSSS!")}
